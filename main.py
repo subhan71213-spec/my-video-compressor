e66747cb66e5dc7b3db10dcd3c26d2c911e45f16
subhan71213-spec <subhan71213@gmail.com>
2026-04-01 22:07:43 +0530

    Update main.py

--- main.py
+++ main.py
@@ -5,8 +5,8 @@ from pyrogram import Client, filters
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 
 # --- CONFIG ---
-API_ID = "1234567"  # my.telegram.org se lein
-API_HASH = "abcdef123456" # my.telegram.org se lein
+API_ID = int(os.getenv("API_ID"))
+API_HASH = os.getenv("API_HASH")
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 
 app = Client("my_bot", api_id=API_ID, api_hash=API_HASH, bot_token=BOT_TOKEN)
